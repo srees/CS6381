6fed3a7f4d310d36de755c79957e34e008f09970
Stephen Rees <900-srees@users.noreply.git.isis.vanderbilt.edu>
2022-02-04 22:32:16 -0600

    playing with ip detection

--- publicip.py
+++ publicip.py
@@ -5,7 +5,7 @@ import os
 def get_ip_address():
     hostname = socket. gethostname()
     local_ip = socket. gethostbyname(hostname)
-    if local_ip != '127.0.0.1':
+    if not local_ip.startswith('127'):
         return local_ip
     else:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -14,13 +14,13 @@ def get_ip_address():
         except socket.error:
             pass
         local_ip = s.getsockname()[0]
-        if local_ip != '127.0.0.1':
+        if not local_ip.startswith('127'):
             return local_ip
         else:
             stream = os.popen("ifconfig | grep 'inet ' | grep -v 127.0.0.1 | awk '$1 == \"inet\" {print $2}'")
             output = stream.read()
             if output == '':
                 print("Warning, using loopback!")
-                output = '127.0.0.1'
+                output = local_ip
             return output
                 
