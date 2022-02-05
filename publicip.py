e8be3aac1964f74b76bc1e6e54e96672ca030321
Stephen Rees <297601+srees@users.noreply.github.com>
2022-02-04 22:05:44 -0600

    Update publicip.py

--- publicip.py
+++ publicip.py
@@ -1,10 +1,26 @@
 import socket
+import os
 
 
 def get_ip_address():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        s.connect(("8.8.8.8", 80))
-    except socket.error:
-        pass
-    return s.getsockname()[0]
+    hostname = socket. gethostname()
+    local_ip = socket. gethostbyname(hostname)
+    if local_ip != '127.0.0.1'
+        return local_ip
+    else:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            s.connect(("8.8.8.8", 80))
+        except socket.error:
+            pass
+        local_ip = s.getsockname()[0]
+        if local_ip != '127.0.0.1'
+            return local_ip
+        else:
+            stream = os.popen("ifconfig | grep 'inet ' | grep -v 127.0.0.1 | awk '$1 == \"inet\" {print $2}'")
+            output = stream.read()
+            if output == ''
+                print("Warning, using loopback!")
+                output = '127.0.0.1'
+            return output
+                
