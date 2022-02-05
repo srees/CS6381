3f95077e2b92bbd574195cc3f7c3c9878c1ec3c7
Stephen Rees <900-srees@users.noreply.git.isis.vanderbilt.edu>
2022-02-04 23:03:38 -0600

    playing with ip detection

--- publicip.py
+++ publicip.py
@@ -3,18 +3,18 @@ import os
 
 
 def get_ip_address():
-    hostname = socket. gethostname()
-    host_ip = socket. gethostbyname(hostname)
-    print('gethost: ' + host_ip)
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        s.connect(("8.8.8.8", 80))
-    except socket.error:
-        pass
-    sock_ip = s.getsockname()[0]
-    print('socketname: ' + sock_ip)
+    # hostname = socket. gethostname()
+    # host_ip = socket. gethostbyname(hostname)
+    # print('gethost: ' + host_ip)
+    # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # try:
+    #     s.connect(("8.8.8.8", 80))
+    # except socket.error:
+    #     pass
+    # sock_ip = s.getsockname()[0]
+    # print('socketname: ' + sock_ip)
     stream = os.popen("ifconfig | grep 'inet ' | grep -v 127.0.0.1 | awk '$1 == \"inet\" {print $2}'")
     os_ip = stream.read().strip()
-    print('ifconfig: ' + os_ip)
+    # print('ifconfig: ' + os_ip)
     return os_ip
                 
