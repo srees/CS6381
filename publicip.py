8dc4e6cf1d056ba63f5c4a1647c687bb795a99c7
Stephen Rees <900-srees@users.noreply.git.isis.vanderbilt.edu>
2022-02-04 22:51:39 -0600

    playing with ip detection

--- publicip.py
+++ publicip.py
@@ -4,23 +4,17 @@ import os
 
 def get_ip_address():
     hostname = socket. gethostname()
-    local_ip = socket. gethostbyname(hostname)
-    if not local_ip.startswith('127'):
-        return local_ip
-    else:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        try:
-            s.connect(("8.8.8.8", 80))
-        except socket.error:
-            pass
-        local_ip = s.getsockname()[0]
-        if not local_ip.startswith('127'):
-            return local_ip
-        else:
-            stream = os.popen("ifconfig | grep 'inet ' | grep -v 127.0.0.1 | awk '$1 == \"inet\" {print $2}'")
-            output = stream.read()
-            if output == '':
-                print("Warning, using loopback!")
-                output = local_ip
-            return output
+    host_ip = socket. gethostbyname(hostname)
+    print('gethost: ' + host_ip)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+    except socket.error:
+        pass
+    sock_ip = s.getsockname()[0]
+    print('socketname: ' + sock_ip)
+    stream = os.popen("ifconfig | grep 'inet ' | grep -v 127.0.0.1 | awk '$1 == \"inet\" {print $2}'")
+    os_ip = stream.read()
+    print('ifconfig: ' + os_ip)
+    return sock_ip
                 
