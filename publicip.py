4fdb1b0dfaaaa228e108c85ebd207927f2c833df
Stephen Rees <900-srees@users.noreply.git.isis.vanderbilt.edu>
2022-02-04 21:43:12 -0600

    attempt to catch exception

--- publicip.py
+++ publicip.py
@@ -3,5 +3,8 @@ import socket
 
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
+    try:
+        s.connect(("8.8.8.8", 80))
+    except socket.error:
+        pass
     return s.getsockname()[0]
